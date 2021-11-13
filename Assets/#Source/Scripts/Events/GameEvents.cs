1f937a972e35afa6f28cbec4dffd6a6516a08f8b
Yoshua Woo <yoshua.woo@gmail.com>
2021-11-14 00:27:10 +0100

    Event Singletons

---
+++ Assets/#Source/Scripts/Events/GameEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Source.Scripts
+{
+	public class GameEvents : MonoBehaviour
+	{
+		#region Singleton
+		public static GameEvents Instance { get; private set; }
+		private void Awake()
+		{
+			if ( Instance != null && Instance != this )
+			{
+				Destroy( this );
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+		#endregion
+
+		public event Action OnGameLost;
+		public void GameLost(){OnGameLost?.Invoke();}
+
+		public event Action OnPraiseFull;
+		public void PraiseFull(){OnPraiseFull?.Invoke();}
+	}
+}
---
+++ Assets/#Source/Scripts/Events/UIEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Source.Scripts
+{
+	public class UIEvents : MonoBehaviour
+	{
+		#region Singleton
+		public static UIEvents Instance { get; private set; }
+		private void Awake()
+		{
+			if ( Instance != null && Instance != this )
+			{
+				Destroy( this );
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+		#endregion
+
+		public event Action<int> OnModifyEnergy;
+		public void ModifyEnergy(int energyAmount){OnModifyEnergy?.Invoke(energyAmount);}
+
+		public event Action<int> OnModifyPraise;
+		public void ModifyPraise(int praiseAmount){OnModifyPraise?.Invoke(praiseAmount);}
+
+		public event Action<float> OnModifyTimer;
+		public void ModifyTimer(float time){OnModifyTimer?.Invoke(time);}
+	}
+}
