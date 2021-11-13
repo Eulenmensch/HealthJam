9c263d5b6d0823c51081c36b0ed502bda86a895f
Yoshua Woo <yoshua.woo@gmail.com>
2021-11-14 00:27:51 +0100

    WIP Game Manager

---
+++ Assets/#Source/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+public enum GameState
+{
+	OnShift,
+	OffShift,
+	Minigame,
+	Lost
+}
+
+namespace _Source.Scripts
+{
+	public class GameManager : MonoBehaviour
+	{
+		#region Singleton
+		public static GameManager Instance { get; private set; }
+		private void Awake()
+		{
+			DontDestroyOnLoad(transform.gameObject);
+			if ( Instance != null && Instance != this )
+			{
+				Destroy( this );
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+		#endregion
+
+		[field: SerializeField] public PlayerData PlayerData { get; set; }
+		[SerializeField] private ShiftData shiftData;
+
+		private GameState gameState = GameState.OnShift;
+
+		private float timer;
+		private float energyTickTimer;
+
+		private void Start()
+		{
+			timer = 0;
+			energyTickTimer = 0;
+			PlayerData.Energy = PlayerData.maxEnergy;
+		}
+
+		private void Update()
+		{
+			switch (gameState)
+			{
+				case GameState.OnShift:
+					HandleOnShift();
+					break;
+				case GameState.OffShift:
+					HandleOffShift();
+					break;
+				case GameState.Minigame:
+					HandleMinigame();
+					break;
+				case GameState.Lost:
+					HandleGameLost();
+					break;
+				default:
+					Debug.LogError("The GameState is not valid");
+					break;
+			}
+		}
+
+		private void HandleOnShift()
+		{
+			energyTickTimer += Time.deltaTime;
+			if (energyTickTimer >= PlayerData.energyTickRateInSeconds)
+			{
+				PlayerData.Energy --;
+				energyTickTimer = 0;
+			}
+
+			timer += Time.deltaTime;
+
+			float remainingTimeInSeconds = shiftData.ShiftDurationInMinutes * 60 - timer;
+			UIEvents.Instance.ModifyTimer(remainingTimeInSeconds);
+			if (remainingTimeInSeconds <= 0f)
+			{
+				timer = 0;
+				gameState = GameState.OffShift;
+				print("gamestate off shift");
+			}
+		}
+
+		private void HandleOffShift()
+		{
+
+		}
+
+		private void HandleMinigame()
+		{
+
+		}
+
+		private void HandleGameLost()
+		{
+
+		}
+	}
+}
